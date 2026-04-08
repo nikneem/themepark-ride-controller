a52479b2637b1793facc4b4ae55d0abd549b2773
nikneem <ekeilholz+git@outlook.com>
2026-04-08 21:07:39 +0200

    feat: add AspireConstants class for project and Dapr component constants

---
+++ src/Aspire/ThemePark.Aspire.ServiceDefaults/AspireConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace ThemePark.Aspire.ServiceDefaults;
+
+public class AspireConstants
+{
+
+    public sealed class Projects
+    {
+        public const string MaintenanceService = "maintenance-api";
+    }
+
+    public sealed class DaprComponents
+    {
+        public const string PubSub = "themepark-pubsub";
+        public const string StateStore = "themepark-statestore";
+
+    }
+}
